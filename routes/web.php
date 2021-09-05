64beca9860b8e68e0d998986b7348e0065f010aa
levintoo <tooklevin@gmail.com>
2021-09-05 19:41:01 +0300

    list blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('show_all_dishes');
 });
 
 Route::resource('dishes', 'dishController');
